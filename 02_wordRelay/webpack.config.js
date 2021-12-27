c843aa40598841acdab02b81e013c4c8e808e4fc
Yunsol <c21154035@gmail.com>
2021-12-27 21:49:01 +0900

    끝말잇기까지

--- 02_wordRelay/webpack.config.js
+++ 02_wordRelay/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name: 'wordrelay-setting',
@@ -15,13 +17,27 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties']
+                presets: [['@babel/preset-env', {
+                    targets:{
+                        browsers: ['> 1% in KR'], 
+                    }
+                    ,debug:true
+                }], '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties', 'react-refresh/babel']
             }
         }]
     },
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
-    } //출력
+        filename: 'app.js',
+        publicPath: '/dist/',
+    }, //출력
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true
+    }
 };
