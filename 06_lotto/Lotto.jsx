85f20d99a1f2df1a6e3df69d9c92cc9b2d3710a1
Yunsol <c21154035@gmail.com>
2022-01-04 22:10:03 +0900

    설명추가

--- 06_lotto/Lotto.jsx
+++ 06_lotto/Lotto.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect, useMemo, useCallback } from "react";
 // useMemo : 복잡한 함수 결과값을 기억
 // useRef : 일반값을 기억
+// useCallback: 함수 자체를 기억
 import Ball from "./Ball";
 
 function getWinNumbers() {
@@ -20,13 +21,26 @@ function getWinNumbers() {
 }
 
 const Lotto = () => {
+    // Hooks를 선언할때 조건문 안에 넣으면 안되고 반복문도 웬만하면 넣지 말기(순서가 확실히 정해진 반복문은 괜찮지만..)
+    // 두번쨰 요소에 넣는값이 바뀌지 않는이상 리턴값을 계속 기억해둠
     const lottoNumbers = useMemo(() => getWinNumbers(), []); // Hooks는 전체를 다시 실행하는데 useMemo를 사용하면 값을 기억하고 있어서 해당 값은 함수호출을 안함
-    const [winNumbers, setWinNumbers] = useState(lottoNumbers);
     const [winBalls, setWinBalls] = useState([]);
+    const [winNumbers, setWinNumbers] = useState(lottoNumbers);
     const [bonus, setBonus] = useState(null);
     const [redo, setRedo] = useState(false);
     const timeouts = useRef([]);
 
+    // componentDidUpdate에만 하고싶을 때 (패턴 외우삼)
+    // const mounted = useRef(false);
+    // useEffect(() => {
+    //     if (!mounted.current) {
+    //         mounted.current = true;
+    //     } else {
+    //         // componentDidUpdate에서만 하고싶은거..  ajax호출 등...
+    //     }
+    // }, [바뀌는 값]);
+    
+    //useEffect안에 useState쓰지말기
     useEffect(() => {
         if (winBalls.length === 0) {
             runTimeouts();
@@ -53,13 +67,16 @@ const Lotto = () => {
             setRedo(true);
         }, 7000);
     };
-    const onClickRedo = () => {
+    const onClickRedo = useCallback(() => {
         setWinNumbers(getWinNumbers());
         setWinBalls([]);
         setBonus(null);
         setRedo(false);
         timeouts.current = [];
-    };
+    }, []);
+    // 함수컴포넌트는 전체가 다시 실행되는데 함수 자체를 기억해서 함수컴포넌트가 재실행되도 onClickRedo를 재생성하지 않음
+    // 첫번째 state를 계속 기억함...state를 사용할때는 두번쨰 인자에 넣어주어야 변경된 값을 기억함
+    // useCallback 필수로 적용해야할때: 자식컴포넌트에 함수를 넘길때 필수! useCallback을 안쓸때 자식한테 새로운 함수를 준다고 생각해서 새로 렌더링을 함
 
     return (
         <>
